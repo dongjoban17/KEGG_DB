fee68957713827dad02d0219b86dc2b9304ec9d1
dongjoban17 <dongjoban17@gmail.com>
2018-02-16 08:45:36 -0500

    minor adjustments

---
+++ etc_scripts/KEGG_Gene/dloadGeneInfo.py
@@ -0,0 +1,37 @@
+"""#####################################
+
+Dongjo Ban
+Jordan Lab 2017
+Create KEGG DB for CoMet
+Python 2.x
+
+"""######################################
+
+import requests, json, re, sys
+from bioservices.kegg import KEGG
+import xml.etree.ElementTree as ET
+
+
+def main():
+
+	# Start KEGG interface
+	k = KEGG()
+	# Create a dict to store final result
+	data = dict()
+
+	# Read in KEGG gene ID & gene symbol pairs
+	with open("hsa_gene_list.json","r") as g:
+		gene_data = json.load(g)
+
+	for gene in gene_data.keys():
+		print gene
+		g_data = k.get(gene)
+		g_prsd = k.parse(g_data)
+		
+		data[gene] = g_prsd
+
+	with open ('ginfo.json','w') as fw:
+		json.dump(data, fw)
+
+if __name__ == "__main__":
+	main()
