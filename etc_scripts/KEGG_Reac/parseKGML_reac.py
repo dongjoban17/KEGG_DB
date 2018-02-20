03bf28bac225d94a9b4e8d43f99afc4f6e105d43
dongjoban17 <dongjoban17@gmail.com>
2018-02-19 19:37:15 -0500

    fix scripts

--- etc_scripts/KEGG_Reac/parseKGML_reac.py
+++ etc_scripts/KEGG_Reac/parseKGML_reac.py
@@ -16,22 +16,34 @@ list_path = open("hsa_list.txt").read().replace('path:','').split('\n') # create
 list_path.pop() # random blank entry removed
 
 i = 0
+dup = {}
+reac = {}
 for hsa in list_path:
 	i+=1
 	print("# of pathways processed: ",i)
 
-	req_url = 'http://rest.kegg.jp/get/'+hsa+'/kgml'
-	kmgl = requests.get(req_url)
-	root = ET.fromstring(kmgl.text)
+	# req_url = 'http://rest.kegg.jp/get/'+hsa+'/kgml'
+	# kmgl = requests.get(req_url)
+	kgml = open("../KEGG_DB_PATH/pathways/path_"+hsa).read()
+	root = ET.fromstring(kgml)
 	
 	for reaction in root.findall("./reaction"): # ALL 'reaction' children of the root
-
-
 		r_ids = [x.strip() for x in reaction.attrib["name"].split('rn:')[1:]]
-
 		for r in r_ids:
-			data[r] = reaction.attrib["type"]
-			print r,data[r]
+			if r in data.keys():
+				if data[r][0] != reaction.attrib['type']:
+					print "not matching", r, data[r], reaction.attrib['type']
+					# for counting reactions with 2 directions
+					dup[r] = 0
+					# consider reactions with conflicting reversibility to be irreversible
+					data[r] = ["irreversible","YES"]
+			else:
+				data[r] = [reaction.attrib["type"],"NO"]
+			# for counting total #
+			reac[r] = 0
+
+print "number of inconsistencies", len(dup.keys())
+print "total number of reacions", len(reac.keys())
 
 with open('KEGG_Reac.json','w') as f:
 	json.dump(data, f)
--- parseKGML.py
+++ parseKGML.py
@@ -21,20 +21,20 @@ def get_react(gene_reacs):
 	return r_list
 
 # Retrieves gene names (and aliases) for KEGG IDs
-def get_gene(KEGG, gene_ids):
-	g_list=[]
-	for id in gene_ids:
-		# Get initial data from KEGG using ID
-		g_data = KEGG.get(id)
-		# Parse the information retrieved
-		g_parsed = KEGG.parse(g_data)
-		if 'NAME' in g_parsed:
-			# if NAME key exists, append primary gene symbol and known aliases (if any) to the list
-			g_list.append([re.split(', ',g_parsed['NAME'][0])[0], re.split(', ',g_parsed['NAME'][0])[1:]])
-		else:
-			# Handle cases where an ID does not map to a symbol
-			g_list.append('hsa:'+g_parsed['ENTRY'][0].split(' ')[0])
-	return g_list
+# def get_gene(gene_data, gene_ids):
+# 	g_list=[]
+# 	for id in gene_ids:
+# 		# Get initial data from KEGG using ID
+# 		g_data = KEGG.get(id)
+# 		# Parse the information retrieved
+# 		g_parsed = KEGG.parse(g_data)
+# 		if 'NAME' in g_parsed:
+# 			# if NAME key exists, append primary gene symbol and known aliases (if any) to the list
+# 			g_list.append([re.split(', ',g_parsed['NAME'][0])[0], re.split(', ',g_parsed['NAME'][0])[1:]])
+# 		else:
+# 			# Handle cases where an ID does not map to a symbol
+# 			g_list.append('hsa:'+g_parsed['ENTRY'][0].split(' ')[0])
+# 	return g_list
 
 # Retrieves metabolite information from a reaction
 def get_metabs(KEGG, reac_id):
@@ -73,22 +73,23 @@ def main():
 	# Random blank entry removed
 	list_path.pop()
 
-	# Read in KEGG gene ID & gene symbol pairs
-	with open("hsa_gene_list.json","r") as g:
-		gene_data = json.load(g)
 	# Read in KEGG reaction ID & reversibility information
 	with open("KEGG_Reac.json","r") as fp:
 		reac_data = json.load(fp)
+	# Read in gene data from KEGG
+	with open("ginfo.json","r") as fp2:
+		gene_data = json.load(fp2)
 
 	i = 0
 	for hsa in list_path:
 		i+=1
 		print "# of pathways processed: ",i
 		# Request KGML file for a pathway
-		req_url = 'http://rest.kegg.jp/get/'+hsa+'/kgml'
-		kmgl = requests.get(req_url)
+		# req_url = 'http://rest.kegg.jp/get/'+hsa+'/kgml'
+		# kmgl = requests.get(req_url)
+		kgml = open("etc_scripts/KEGG_DB_PATH/pathways/path_"+hsa).read()
 		# Construct element tree from the requested info
-		root = ET.fromstring(kmgl.text)
+		root = ET.fromstring(kgml)
 		
 		# Iterate through ALL reactions
 		for reaction in root.findall("./reaction"):
